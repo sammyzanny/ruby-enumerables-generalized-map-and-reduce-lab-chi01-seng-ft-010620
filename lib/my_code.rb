a278e0e4135067061f3949b8780b93c1b9896aa8
Samuel Zandi <samuelczandi@gmail.com>
2020-01-03 03:00:38 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(array)
+  new = []
+  i = 0
+  while i < array.length
+    new.push(yield(array[i]))
+    i += 1
+  end
+  new
+end
+
+def reduce(s, sp=nil)
+  if sp
+    accum = sp
+    i = 0
+  else
+    accum = s[0]
+    i = 1
+  end
+  while i < s.length
+    accum = yield(accum, s[i])
+    i += 1
+  end
+  accum
+end
